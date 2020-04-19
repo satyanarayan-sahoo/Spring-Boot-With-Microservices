47947ad457c4ab02640d1fc57626340012e0c100
Satyanarayan Sahoo <59027101+satyanarayan-sahoo@users.noreply.github.com>
2020-04-19 21:50:13 +0530

    Add files via upload

---
+++ Spring Boot+ Eureka Server + Eureka Client + Rest Template + @LoadBalanced/Hello_Service/src/main/java/com/microservice/eurekaClient/HelloServiceApplication.java
@@ -0,0 +1,15 @@
+package com.microservice.eurekaClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}//main
+
+}//class
